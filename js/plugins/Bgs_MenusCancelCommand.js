05db3c55d04594dd47faa5d33c00afea1a8799b2
AdanRafli <adanrafli99@gmail.com>
2022-03-31 13:40:56 +0700

    update 1.5

--- js/plugins/Bgs_MenusCancelCommand.js
+++ js/plugins/Bgs_MenusCancelCommand.js
@@ -1,3 +1,4 @@
+
 //=============================================================================
 // Bgs_MenusCancelCommand.js
 //=============================================================================
@@ -6,22 +7,25 @@
  * @plugindesc Add a cancel command to menus of your choice
  * @author BallgameStudios.com
  *
- * @help This plugin adds a cancel command to menus, including in shops, so the user will
- * never have to press the cancel button (Esc / right-click / two-finger-tap).
+ * @help This plugin adds a cancel command to menus, including in shops, so the user
+ * will never have to press Esc / mouse right-click / two-finger-tap.
  * 
  * The cancel command name is configurable. You may call it "Cancel", "Back",
  * "Exit", or anything else. The menus in which the command appears can also
  * be selected.
  * 
+ * Bonus: You can also change the menus behavior to select items upon a single
+ * click instead of two clicks.
+ * 
  * Terms of use:
  * ------------
  *   Free for non-commercial and commercial use. 
- *   Please credit BallgameStudios.com
+ *   Please credit BallgameStudios
  *
  * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
  * Update log:
  *   1.0: Initial release.
- *   1.1: Exit shop by buying / selling 0 items
+ *   1.1: Support plugins with vertical menus (YEP_ItemCore and YEP_EquipCore)
  * 
  * @param Cancel Command Name
  * @text Cancel Command Name
@@ -30,6 +34,18 @@
  * Leave blank to keep hidden.
  * @default Back
  * 
+ * @param Single Click Menus
+ * @text Single-click Menus
+ * @type boolean
+ * @desc Select true to change menus behavior to select an item in a single click instead of two.
+ * @default false
+ * 
+ * @param Vertical Menus
+ * @text Vertical Menus
+ * @type boolean
+ * @desc Select true if you are using a plugin that changes the item and equipment category menus to vertical ones.
+ * @default false
+ *  
  * @param Include in Main Menu
  * @text Include in Main Menu
  * @type boolean
@@ -87,7 +103,7 @@
  */
 
 var Imported = Imported || {};
-Imported.Bgs_MenusCancelCommand = 1.0;
+Imported.Bgs_MenusCancelCommand = 1.1;
 
 var BGS = BGS || {};
 BGS.MenusCancelCommand = BGS.MenusCancelCommand || {};
@@ -99,6 +115,8 @@ BGS.MenusCancelCommand = BGS.MenusCancelCommand || {};
     // Process user parameters
     _.params = PluginManager.parameters('Bgs_MenusCancelCommand');
     _.CancelCommandName = String(_.params['Cancel Command Name'] || '');
+    _.SingleClickMenus = String(_.params['Single Click Menus'] || "false");
+    _.VerticalMenus = String(_.params['Vertical Menus'] || "false");
     _.IncludeInMainMenu = String(_.params['Include in Main Menu'] || "true");
     _.IncludeInItem = String(_.params['Include in Item'] || "true");
     _.IncludeInSkill = String(_.params['Include in Skill'] || "true");
@@ -109,7 +127,45 @@ BGS.MenusCancelCommand = BGS.MenusCancelCommand || {};
     _.IncludeInParty = String(_.params['Include in Party'] || "true");
     _.IncludeInShop = String(_.params['Include in Shop'] || "true");
     
+
+    //-----------------------------------------------------------------------------
+    // Single-click menus
+    //
+    // The window for selecting a command on the main menu screen.
     
+    _.Window_Selectable_onTouch = Window_Selectable.prototype.onTouch;
+    Window_Selectable.prototype.onTouch = function (triggered) {
+        if (_.SingleClickMenus != "true")
+            _.Window_Selectable_onTouch.call(this, triggered);
+        else {
+            var lastIndex = this.index();
+            var x = this.canvasToLocalX(TouchInput.x);
+            var y = this.canvasToLocalY(TouchInput.y);
+            var hitIndex = this.hitTest(x, y);
+            if (hitIndex >= 0) {
+                if (hitIndex === this.index()) {
+                    if (triggered && this.isTouchOkEnabled()) {
+                        this.processOk();
+                    }
+                } else if (this.isCursorMovable()) {
+                    this.select(hitIndex);
+                    if (this.isTouchOkEnabled())    // BGS addition to original function
+                        this.processOk();           // BGS addition to original function
+                }
+            } else if (this._stayCount >= 10) {
+                if (y < this.padding) {
+                    this.cursorUp();
+                } else if (y >= this.height - this.padding) {
+                    this.cursorDown();
+                }
+            }
+            if (this.index() !== lastIndex) {
+                //SoundManager.playCursor();    // BGS commented out
+            }
+        }        
+    };
+
+
     //-----------------------------------------------------------------------------
     // Window_MenuCommand
     //
@@ -141,7 +197,7 @@ BGS.MenusCancelCommand = BGS.MenusCancelCommand || {};
     _.Window_ItemCategory_maxCols = Window_ItemCategory.prototype.maxCols;
     Window_ItemCategory.prototype.maxCols = function () {
         let cols = _.Window_ItemCategory_maxCols.call(this);
-        cols += _.IncludeInItem == "true" ? 1 : 0;
+        cols += (_.IncludeInItem == "true" && _.VerticalMenus == "false")? 1 : 0;
         return cols;
     };
         
@@ -172,7 +228,7 @@ BGS.MenusCancelCommand = BGS.MenusCancelCommand || {};
     Window_ItemList.prototype.makeItemList = function () {
         _.Window_ItemList_makeItemList.call(this);
         if (_.IncludeInItem == "true")
-            this._data.push(_.CancelCommandName);
+            this._data.push({ name: _.CancelCommandName, infoTextTop: '', infoTextBottom: '' });
     };
 
     _.Window_ItemList_drawItem = Window_ItemList.prototype.drawItem;
@@ -181,7 +237,7 @@ BGS.MenusCancelCommand = BGS.MenusCancelCommand || {};
         var rect = this.itemRect(index);
         var priceWidth = 96;
         rect.width -= this.textPadding();
-        if (item == _.CancelCommandName) {
+        if (item != null && item.name == _.CancelCommandName) {
             var iconBoxWidth = Window_Base._iconWidth + 4;
             this.resetTextColor();
             this.drawText(_.CancelCommandName, rect.x + iconBoxWidth, rect.y, rect.width - iconBoxWidth);
@@ -192,7 +248,7 @@ BGS.MenusCancelCommand = BGS.MenusCancelCommand || {};
 
     _.Window_ItemList_processOk = Window_ItemList.prototype.processOk;
     Window_ItemList.prototype.processOk = function () {
-        if (this.item() != null && this.item() == _.CancelCommandName) {
+        if (this.item() != null && this.item().name == _.CancelCommandName) {
             this.processCancel();
         }
         else {
@@ -280,7 +336,7 @@ BGS.MenusCancelCommand = BGS.MenusCancelCommand || {};
     _.Window_EquipCommand_maxCols = Window_EquipCommand.prototype.maxCols;
     Window_EquipCommand.prototype.maxCols = function () {
         let cols = _.Window_EquipCommand_maxCols.call(this);
-        cols += _.IncludeInEquip == "true" ? 1 : 0;
+        cols += (_.IncludeInEquip == "true" && _.VerticalMenus == "false")? 1 : 0;
         return cols;
     };
 
@@ -351,18 +407,42 @@ BGS.MenusCancelCommand = BGS.MenusCancelCommand || {};
     };
 
     _.Window_Status_refresh = Window_Status.prototype.refresh;
-    Window_Status.prototype.refresh = function () {
-        _.Window_Status_refresh.call(this);
+    Window_Status.prototype.refresh = function () {        
         if (this._actor && _.IncludeInStatus == "true") {
             var lineHeight = this.lineHeight();
-            this.drawText(_.CancelCommandName, 6, lineHeight * 15);
+            this.drawText(_.CancelCommandName, 6, lineHeight * 0);
+            this.drawHorzLine(lineHeight * 1);            
+            this.drawBlock1(lineHeight * 2);
+            this.drawHorzLine(lineHeight * 3);
+            this.drawBlock2(lineHeight * 4);
+            this.drawHorzLine(lineHeight * 8);
+            this.drawBlock3(lineHeight * 9);
+            this.drawHorzLine(lineHeight * 15);
+            this.drawBlock4(lineHeight * 16);
+            this.select(0);
         }
+        else
+            _.Window_Status_refresh.call(this);
+    };
+
+    _.Window_Status_numVisibleRows = Window_Status.prototype.numVisibleRows;
+    Window_Status.prototype.numVisibleRows = function () {
+        let max = _.Window_Status_numVisibleRows.call(this);
+        max += _.IncludeInStatus == "true" ? 1 : 0;
+        return max;
+    };
+
+    _.Window_Status_maxItems = Window_Status.prototype.maxItems;
+    Window_Status.prototype.maxItems = function () {
+        let max = _.Window_Status_maxItems.call(this);
+        max += _.IncludeInStatus == "true" ? 1 : 0;
+        return max;        
     };
 
     Window_Status.prototype.processOk = function () {
         this.callOkHandler();
-    };
-
+    };        
+    
 
     //-----------------------------------------------------------------------------
     // Window_Options
@@ -514,35 +594,5 @@ BGS.MenusCancelCommand = BGS.MenusCancelCommand || {};
             _.Window_ShopBuy_processOk.call(this);
         }       
     };
-
-
-
-    //-----------------------------------------------------------------------------
-    // Window_ShopNumber
-    //
-    // The window for inputting quantity of items to buy or sell on the shop
-    // screen.
-
-    _.Window_ShopNumber_setup = Window_ShopNumber.prototype.setup;
-    Window_ShopNumber.prototype.setup = function (item, max, price) {
-        this._item = item;
-        this._max = Math.floor(max);
-        this._price = price;
-        this._number = 0; // changed from 1 to 0
-        this.placeButtons();
-        this.updateButtonsVisiblity();
-        this.refresh();
-    };
-
-    _.WindowWindow_ShopNumber_changeNumber = Window_ShopNumber.prototype.changeNumber;
-    Window_ShopNumber.prototype.changeNumber = function (amount) {
-        var lastNumber = this._number;
-        this._number = (this._number + amount).clamp(0, this._max); // changed the clamp from 1 to 0
-        if (this._number !== lastNumber) {
-            SoundManager.playCursor();
-            this.refresh();
-        }
-    };
-
 
 })(BGS.MenusCancelCommand);
